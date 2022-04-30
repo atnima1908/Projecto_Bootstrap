6334aa4d56fefe429a49a87c2024069d1598430d
atnima1908 <aminta.sanchez.largaespada@ucreativa.com>
2022-04-30 12:42:42 -0400

    Configuracion de Bootstrap 5

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+// OFF CANVAS COLLAPSE - NAV BAR //
+(function mainScript() {
+  "use strict";
+
+  const offcanvasToggle = document.querySelector(
+    '[data-bs-toggle="offcanvas"]',
+  );
+  const offcanvasCollapse = document.querySelector(".offcanvas-collapse");
+
+  offcanvasToggle.addEventListener("click", function () {
+    offcanvasCollapse.classList.toggle("open");
+  });
+})();
+
+
+
